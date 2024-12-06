f40cd071145b6bac06166581eabf806cf14e233c
Nazan Top <nazantop@gmail.com>
2024-12-06 20:21:05 +0100

    fix

--- ExchangeRateAPI/obj/Debug/net9.0/ExchangeRateAPI.AssemblyInfo.cs
+++ ExchangeRateAPI/obj/Debug/net9.0/ExchangeRateAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExchangeRateAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+981854c3869acd82946a77097c805a127874b99a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExchangeRateAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExchangeRateAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
